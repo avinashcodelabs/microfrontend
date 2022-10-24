5d6770658d20360e96b6431046f0a32513d75c94
avinash2wards <avinash2wards@outlook.com>
2022-10-24 08:42:12 +0530

    Initial commit

---
+++ react-mfe/container/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Container Header</h1>
+      <hr></hr>
+      <MarketingApp />
+    </div>
+  );
+};
+
+export default App;
