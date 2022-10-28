d59fd4d721eb87028a510b91fb0aac800f1e32ac
avinash2wards <avinash2wards@outlook.com>
2022-10-28 08:33:19 +0530

    added dashboard vue subapp

--- react-mfe/container/src/App.js
+++ react-mfe/container/src/App.js
@@ -1,7 +1,13 @@
 import React, { Suspense, lazy, useState } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+  Router,
+} from "react-router-dom";
 import Progress from "./components/Progress";
-
+import { createBrowserHistory } from "history";
 import {
   StylesProvider,
   createGenerateClassName,
@@ -14,11 +20,19 @@ const generateClassName = createGenerateClassName({
 
 const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 const AuthLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
+
+const history = createBrowserHistory();
 
 const App = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
+
+  React.useEffect(() => {
+    if (isSignedIn) history.push("/dashboard");
+  }, [isSignedIn]);
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header
@@ -30,12 +44,16 @@ const App = () => {
               <Route path="/auth">
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardLazy />
+              </Route>
               <Route path="/" component={MarketingLazy}></Route>
             </Switch>
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
 
